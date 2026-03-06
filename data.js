f1fd628bf2cd9937188f47cc6e12ec48ab82be80
Mugshot1981 <joasdewit@gmail.com>
2026-03-06 10:48:36 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,143 @@
 
+const chapters = {
+
+"Hoofdstuk 4 – De Tweede Wereldoorlog 1939-1945 en dekolonisatie": [
+
+{
+term: "Asmogendheden",
+description: "Het bondgenootschap van Duitsland, Italië en Japan tijdens de Tweede Wereldoorlog."
+},
+
+{
+term: "Blitzkrieg",
+description: "Duits voor bliksemoorlog. De snelle Duitse opmars tijdens de Tweede Wereldoorlog."
+},
+
+{
+term: "Slag om Engeland",
+description: "Een felle luchtoorlog tussen Duitsland en Groot-Brittannië."
+},
+
+{
+term: "Operatie Barbarossa",
+description: "Het Duitse plan om het westelijke deel van de Sovjet-Unie in te nemen."
+},
+
+{
+term: "Geallieerden",
+description: "Het bondgenootschap dat vocht tegen de asmogendheden, waaronder Groot-Brittannië, de Sovjet-Unie en de Verenigde Staten."
+},
+
+{
+term: "Slag om Stalingrad",
+description: "Een veldslag bij de Russische stad Stalingrad in 1942-1943 waarbij Duitsland een zware nederlaag leed."
+},
+
+{
+term: "Pearl Harbor",
+description: "Een Amerikaanse marinebasis op Hawaï die in 1941 werd aangevallen door Japan."
+},
+
+{
+term: "D-day",
+description: "De grote invasie in Normandië (Frankrijk) op 6 juni 1944 door de geallieerden."
+},
+
+{
+term: "Tweefrontenoorlog",
+description: "Een oorlog waarbij een land op twee fronten tegelijk moet vechten."
+},
+
+{
+term: "Duitse bezetting",
+description: "De periode van 10 mei 1940 tot 5 mei 1945 waarin Duitsland de macht had in Nederland."
+},
+
+{
+term: "Capitulatie",
+description: "Het leger legt de wapens neer en geeft zich over."
+},
+
+{
+term: "Regering in ballingschap",
+description: "Een regering die zegt de macht te hebben in een land maar deze daar niet kan uitoefenen."
+},
+
+{
+term: "Gelijkschakeling",
+description: "Het onder directe controle van de nazi’s brengen van alle organisaties en media."
+},
+
+{
+term: "Rechtsstaat",
+description: "Een staat waarin burgers beschermd zijn tegen onrechtmatig optreden van de overheid en van andere burgers."
+},
+
+{
+term: "Collaboratie",
+description: "Samenwerken met de bezetter."
+},
+
+{
+term: "Verzet",
+description: "De bezetter tegenwerken."
+},
+
+{
+term: "Persoonsbewijs",
+description: "Een identiteitsbewijs."
+},
+
+{
+term: "Februaristaking",
+description: "Een grote staking in Amsterdam in 1941 tegen anti-Joodse maatregelen van de bezetter."
+},
+
+{
+term: "Distributiestelsel",
+description: "Een systeem waarbij je alleen producten kunt kopen met bonkaarten."
+},
+
+{
+term: "Operatie Market Garden",
+description: "Een plan van de geallieerden om via Nederland Duitsland binnen te vallen door bruggen over grote rivieren in te nemen."
+},
+
+{
+term: "Slag om Arnhem",
+description: "De poging van de geallieerden in 1944 om de brug over de Rijn bij Arnhem te veroveren op de Duitsers."
+},
+
+{
+term: "Spoorwegstaking",
+description: "Een staking van het Nederlandse treinpersoneel in 1944 om het Duitse leger te verzwakken."
+},
+
+{
+term: "Hongerwinter",
+description: "De strenge winter van 1944-1945 waarin er hongersnood was in West- en Noord-Nederland."
+},
+
+{
+term: "Antisemitisme",
+description: "Haat tegen Joden."
+},
+
+{
+term: "Rassenleer",
+description: "Het idee van de nazi’s dat er verschillende mensenrassen zijn en dat het ene ras beter is dan het andere."
+},
+
+{
+term: "Neurenberger rassenwetten",
+description: "Wetten die vanaf 1935 in nazi-Duitsland golden waardoor Joden niet meer als volwaardige burgers werden gezien."
+},
+
+{
+term: "Kristallnacht",
+description: "Een door de nazi’s georganiseerde actie in november 1938 waarbij Joodse mensen werden aangevallen en Joods bezit werd vernield."
+}
+
+]
+
+};
