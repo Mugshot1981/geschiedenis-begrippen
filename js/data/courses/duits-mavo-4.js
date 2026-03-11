bb1acddfcc270f6bbafe9784cf4d745d6d442f95
Mugshot1981 <joasdewit@gmail.com>
2026-03-11 08:58:57 +0100

    Update duits-mavo-4.js

--- js/data/courses/duits-mavo-4.js
+++ js/data/courses/duits-mavo-4.js
@@ -1,1 +1,136 @@
 
+/*
+KennisSprint dataset
+© Joas de Wit
+2026
+*/
+
+export const duitsMavo4Course = {
+  id: "duits-mavo-4",
+  subject: "duits",
+  level: "mavo",
+  grade: "4",
+  title: "Duits 4 mavo",
+
+  chapters: [
+    {
+      id: "deu-h1",
+      subjectId: "duits",
+      title: "Essen und Trinken"
+    },
+    {
+      id: "deu-h2",
+      subjectId: "duits",
+      title: "Körper"
+    },
+    {
+      id: "deu-h3",
+      subjectId: "duits",
+      title: "Freizeit"
+    },
+    {
+      id: "deu-h4",
+      subjectId: "duits",
+      title: "Menschen"
+    },
+    {
+      id: "deu-h5",
+      subjectId: "duits",
+      title: "Schule"
+    },
+    {
+      id: "deu-h6",
+      subjectId: "duits",
+      title: "Arbeit"
+    },
+    {
+      id: "deu-h7",
+      subjectId: "duits",
+      title: "Gefühle"
+    },
+    {
+      id: "deu-h8",
+      subjectId: "duits",
+      title: "Kunst und Kultur"
+    },
+    {
+      id: "deu-h9",
+      subjectId: "duits",
+      title: "Tourismus"
+    },
+    {
+      id: "deu-h10",
+      subjectId: "duits",
+      title: "Umgebung"
+    },
+    {
+      id: "deu-h11",
+      subjectId: "duits",
+      title: "Kontakte"
+    },
+    {
+      id: "deu-h12",
+      subjectId: "duits",
+      title: "Innovation"
+    },
+    {
+      id: "deu-h13",
+      subjectId: "duits",
+      title: "Geld"
+    },
+    {
+      id: "deu-h14",
+      subjectId: "duits",
+      title: "Umwelt"
+    },
+    {
+      id: "deu-h15",
+      subjectId: "duits",
+      title: "Nachhaltigkeit"
+    },
+    {
+      id: "deu-h16",
+      subjectId: "duits",
+      title: "Gemeinschaftskunde"
+    },
+    {
+      id: "deu-h17",
+      subjectId: "duits",
+      title: "Falsche Freunde"
+    },
+    {
+      id: "deu-h18",
+      subjectId: "duits",
+      title: "Signalwörter"
+    },
+    {
+      id: "deu-h19",
+      subjectId: "duits",
+      title: "Prüfungssprache"
+    },
+    {
+      id: "deu-h20",
+      subjectId: "duits",
+      title: "Redemittel"
+    }
+  ],
+
+  datasets: {
+    terms: [],
+    years: [],
+    persons: []
+  },
+
+  modes: [
+    {
+      id: "term-to-answer",
+      label: "Duits → Nederlands",
+      dataset: "terms"
+    },
+    {
+      id: "answer-to-term",
+      label: "Nederlands → Duits",
+      dataset: "terms"
+    }
+  ]
+};
